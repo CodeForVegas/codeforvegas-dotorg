a0f583ccefa4c948b979fe28334a3e0a2b7b2113
Nick Oliveira <87996659+noliveira95@users.noreply.github.com>
2022-08-14 14:31:13 -0700

    Added typing animation

--- components/welcome-to-cfv.vue
+++ components/welcome-to-cfv.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
-    <section class="welcome">
+    <section class="welcome-container">
         <div class="welcome-header">
-            <!-- <h1>Welcome to Code For Vegas</h1> -->
+            <h1>Welcome to Code For Vegas</h1>
         </div>
     </section>
 </div>
@@ -15,16 +15,36 @@ export default {
 </script>
 
 <style>
-.welcome {
-    height: 10rem;
+.welcome-container {
     display: flex;
     justify-content: center;
-    align-items: center;   
+    align-items: center;
+    height: 30rem;
 }
 
 .welcome-header {
-    color: white;
+    color: #fff;
     font-family: 'Roboto Mono', monospace;
     font-weight: 500;
+    animation: typing 2s steps(25), blink 1s step-end infinite;
+    white-space: nowrap;
+    overflow: hidden;
+    border-right: 2px solid #fff;
    }
+
+@keyframes typing {
+  from {
+    width: 0%
+  }
+
+  to {
+    width: 100%;
+  }
+}
+    
+@keyframes blink {
+  50% {
+    border-color: transparent
+  }
+}
 </style>
