632c613a1556f183b07eac5eef768faf20451394
Nick Oliveira <87996659+noliveira95@users.noreply.github.com>
2022-08-14 15:52:39 -0700

    Added content

--- components/about-us.vue
+++ components/about-us.vue
@@ -1,11 +1,49 @@
 <template>
   <div>
-    <section class="about-container">
-        <b-container class="about">
-            <h2>About Us</h2>
-            <p>The Code for Vegas Foundation was rebooted in 2022, formed as a Nevada Nonprofit Corporation on 16 June 2022, and is presently pursuing a 501(c)(3) public charity determination with the IRS.</p>
-        </b-container>
-    </section>
+    <b-container class="about-container">
+            <b-row class="about-header" align-h="center">
+                <h2 id="about-us">About Us</h2>
+            </b-row>
+            <b-row class="about-body">
+                <p>The Code for Vegas Foundation was rebooted in 2022, formed as a Nevada Nonprofit Corporation on 16 June 2022, and is presently pursuing a 501(c)(3) public charity determination with the IRS.</p>
+            </b-row>
+            <b-row class="about-header">
+                <h3>Purpose</h3>
+            </b-row>
+            <b-row class="about-body">
+                <p>To enable and encourage civic ideation, innovation, and implementation to make our communities better.<br><br>
+                While we have kept the name of our predecessor organization formed in 2014, we endeavor to include all comers wishing to contribute and collaborate on these civic projects and goals, without a specific focus on coding.</p>
+            </b-row>
+            <b-row class="about-header">
+                <h3>Ethos</h3>
+            </b-row>
+            <b-row class="about-body">
+                <p>Our core principles are essential:<br><br>
+                <ul>
+                    <li>Inclusivity</li>
+                    <li>Diversity</li>
+                    <li>Accessibility</li>
+                </ul><br>
+                These principles apply not only to project participation, but also to all meetups, gatherings, online publications, project deployments, and ongoing development of any type of work product. We invite all those interested in learning, contributing, and collaborating on civic projects designed to improve our communities, to do so with confidence and conviction!
+                </p>
+            </b-row>
+            <b-row class="about-header">
+                <h3>Mission</h3>
+            </b-row>
+            <b-row class="about-body">
+                <p>We will invite members of the population at large to build an organization centered on civic volunteerism, skills development, and job creation, with a focus on Making our Smart City Smarter, improving Education, and driving our 3-word ethos: Inclusivity, Diversity, Accessibility.</p>
+            </b-row>
+            <b-row class="about-header">
+                <h3>Policies</h3>
+            </b-row>
+            <b-row class="about-body">
+                <p>
+                    Please find the latest versions of our policies on GitHub by visiting the following links:<br><br>
+                    <a href="https://github.com/CodeForVegas/.github/blob/main/CODE_OF_CONDUCT.md">Code of Conduct</a><br>
+                    <a href="https://github.com/CodeForVegas/.github/blob/main/CONTRIBUTING.md">Contributing to Projects</a>
+                </p>
+            </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -16,5 +54,13 @@ export default {
 </script>
 
 <style>
+
+.about-header {
+    padding: 30px 0px 10px 0px;
+}
+
+.about-container {
+    color: #fff;
+}
 
 </style>
